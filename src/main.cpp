3de32adc0ec69b09f13e043f5c0497571af3ed99
riverian <davon.y@outlook.com>
2024-01-23 10:57:14 +0800

    wifi to ros

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,88 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <ESP32Servo.h>
-
+/*
+ *  Code initializes and connects to a WiFi network using given SSID and password, 
+ *  then publishes a "Hello World!" message to a ROS topic "chatter" at regular intervals. 
+ *  Make sure to update the SSID, password, IP and server details as per your network.
+ * 
+ *  Create by Stan Fu on 2023/08/07
+ */
+#include <arduino.h>
+#include "WiFi.h"
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+IPAddress ip(192, 168, 178, 47);
+IPAddress server(172, 17, 0, 2);
+uint16_t serverPort = 11411;
+// const char*  ssid = "FRITZ!Box 7530 JQ";
+// const char*  password = "27635171366830739521";
+// const char*  ssid = "GH034";
+// const char*  password = "chenlaoshiniubi";
 const char*  ssid = "TP-Link_21B5";
 const char*  password = "26796666";
-const int port = 1234; // The port number to listen on
-const int maxDataLength = 256;
+// Be polite and say hello
+char hello[13] = "hello world!";
+uint16_t period = 1000;
+uint32_t last_time = 0;
 
-WiFiServer server(port);
-WiFiClient client;
-int recieved_u;
-#define TopESC_PIN 14
-Servo Topesc;
-uint8_t rx_uint8 = 0;
-uint8_t rx_byte[2] = {0};
-uint16_t rx_uint16 = 0;
+ros::NodeHandle  nh;
+double rx_temp;
+// Make a chatter publisher
+void ListenerCallback(const std_msgs::Float32 &rx_data){
+  char rx[8];
+  rx_temp = rx_data.data;
+  dtostrf(rx_temp, 2, 4, rx);
+  Serial.printf("rev: %s\n",rx); 
+}
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+ros::Subscriber<std_msgs::Float32> listener("top_motor", ListenerCallback);
 
-uint16_t AssembleUInt16(uint8_t highByte, uint8_t lowByte);
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-  Serial.print("IP:   ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+void setupWiFi();
 
-  Topesc.attach(TopESC_PIN, 1000, 2000);
-  Topesc.writeMicroseconds(1500);
+void setup(){
+    Serial.begin(115200);
+    setupWiFi();
+
+    nh.getHardware()->setConnection(server, serverPort);
+    nh.initNode();
+
+    // Another way to get IP
+    Serial.print("ROS IP = ");
+    Serial.println(nh.getHardware()->getLocalIP());
+
+    // Start to be polite
+    nh.advertise(chatter);
+    nh.subscribe(listener);
+
 }
 
-void loop() {
-  if (client.connected()) {
-    while (client.available()) {
-      // Read incoming data from Simulink
-      /* rx 8bit */
-      rx_uint8 = client.read();
-      Serial.printf("Rx: %4d\n",rx_uint8);
-      /* rx 16bit(unverified) */
-      // client.readBytes(rx_byte,2);
-      // AssembleUInt16(rx_byte[0],rx_byte[1]);
-      // Serial.printf("Rx: %4d\n",rx_uint16);
-      Topesc.writeMicroseconds(rx_uint8);
+void loop(){
+  if(millis() - last_time >= period)
+  {
+    last_time = millis();
+    if (nh.connected())
+    {
+      Serial.println("Connected");
+      // Say hello
+      str_msg.data = hello;
+      chatter.publish( &str_msg );
+    } else {
+      Serial.println("Not Connected");
     }
-  } else {
-    // Wait for a new client to connect
-    client = server.available();
   }
+  nh.spinOnce();
+  delay(1);
 }
 
-uint16_t AssembleUInt16(uint8_t highByte, uint8_t lowByte) {
-  return (static_cast<uint16_t>(highByte) << 8) | lowByte;
+
+void setupWiFi()
+{  
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) { delay(500);Serial.print("."); }
+   Serial.print("SSID: ");
+   Serial.println(WiFi.SSID());
+   Serial.print("IP:   ");
+   Serial.println(WiFi.localIP());
 }
+
